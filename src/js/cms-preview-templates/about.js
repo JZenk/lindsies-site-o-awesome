07c284ef5a724d7e527e6288d731e761866ed54f
JZenk <JZenk@users.noreply.github.com>
2020-04-27 19:09:53 -0500

    First-pass (#1)
    * Because it needs a better name.
    
    * Pretty 404
    
    * Remove products
    
    * Make purple
    
    * No more products
    
    * Values is now about
    
    * Update nav
    
    * Colors and logo
    
    * Make CMS match new layout
    
    * ignore VSCode
    
    * Remove contact form
    
    * Remove product button

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -3,7 +3,7 @@ import { List } from 'immutable';
 
 import Jumbotron from "./components/jumbotron";
 
-const MediaBlock = ({heading, text, imageUrl, reverse}) => {
+const MediaBlock = ({ heading, text, imageUrl, reverse }) => {
   const imageContainerClassName = reverse
     ? "ph3-m w-50-m"
     : "ph3-m w-50-m order-last-m";
@@ -18,25 +18,25 @@ const MediaBlock = ({heading, text, imageUrl, reverse}) => {
   </div>;
 };
 
-export default class ValuesPreview extends React.Component {
+export default class AboutPreview extends React.Component {
   render() {
-    const {entry, getAsset} = this.props;
-    
+    const { entry, getAsset } = this.props;
+
     let image = getAsset(entry.getIn(["data", "image"]));
 
     // Bit of a nasty hack to make relative paths work as expected as a background image here
     if (image && !image.fileObj) {
       image = window.parent.location.protocol + "//" + window.parent.location.host + image;
     }
-    
+
     const entryValues = entry.getIn(["data", "values"]);
     const values = entryValues ? entryValues.toJS() : [];
-    
+
     return <div>
       <Jumbotron image={image} title={entry.getIn(["data", "title"])} />
       <div className="bg-off-white pv4">
         <div className="mw7 center ph3 pt4">
-          {values.map(({text, heading, imageUrl}, i) =>
+          {values.map(({ text, heading, imageUrl }, i) =>
             <MediaBlock key={i} text={text} heading={heading} imageUrl={imageUrl} reverse={i % 2 === 0} />
           )}
         </div>
